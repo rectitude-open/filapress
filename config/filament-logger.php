38b4524a419126df0c7edddd882d165f3e5339e2
aspirantzhang <admin@aspirantzhang.com>
2025-04-12 16:00:20 +0800

    fix: update date formats and navigation group in filament logger configuration

--- config/filament-logger.php
+++ config/filament-logger.php
@@ -7,12 +7,12 @@ use Z3d0X\FilamentLogger\Loggers\ResourceLogger;
 use Z3d0X\FilamentLogger\Resources\ActivityResource;
 
 return [
-    'datetime_format' => 'd/m/Y H:i:s',
-    'date_format' => 'd/m/Y',
+    'datetime_format' => 'Y-m-d H:i:s',
+    'date_format' => 'Y-m-d',
 
     'activity_resource' => ActivityResource::class,
     'scoped_to_tenant' => true,
-    'navigation_sort' => null,
+    'navigation_sort' => 9,
 
     'resources' => [
         'enabled' => true,
@@ -24,7 +24,7 @@ return [
             // App\Filament\Resources\UserResource::class,
         ],
         'cluster' => null,
-        'navigation_group' => 'Settings',
+        'navigation_group' => 'Security',
     ],
 
     'access' => [
