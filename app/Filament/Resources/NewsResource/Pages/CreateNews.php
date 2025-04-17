57b1c5885648ba8197716027d7e90d4ea1f57fe0
aspirantzhang <admin@aspirantzhang.com>
2025-04-17 20:03:25 +0800

    feat: enhance CreateNews page with redirect and form action methods

--- app/Filament/Resources/NewsResource/Pages/CreateNews.php
+++ app/Filament/Resources/NewsResource/Pages/CreateNews.php
@@ -3,10 +3,26 @@
 namespace App\Filament\Resources\NewsResource\Pages;
 
 use App\Filament\Resources\NewsResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateNews extends CreateRecord
 {
     protected static string $resource = NewsResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->getCreateFormAction()->formId('form'),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 }
