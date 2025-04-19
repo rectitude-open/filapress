b0debc1eada030ac10d81ff2fb565529086602b2
aspirantzhang <admin@aspirantzhang.com>
2025-04-19 15:40:20 +0800

    fix: correct drop table method in settings migration

--- database/migrations/2025_04_12_193707_create_settings_table.php
+++ database/migrations/2025_04_12_193707_create_settings_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
         });
     }
 
-    public function settings()
+    public function down()
     {
-        Schema::dropIfExists('versions');
+        Schema::dropIfExists('settings');
     }
 };
