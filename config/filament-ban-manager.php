1a173f5ce3a339a541a1542b6ecdfadfd7ba5fa1
aspirantzhang <admin@aspirantzhang.com>
2025-05-02 14:58:28 +0800

    fix: update filament-ban-manager version and correct datetime format in configuration

--- config/filament-ban-manager.php
+++ config/filament-ban-manager.php
@@ -7,5 +7,5 @@ return [
     'model' => RectitudeOpen\FilamentBanManager\Models\Ban::class,
     'navigation_sort' => 0,
     'navigation_icon' => 'heroicon-o-shield-exclamation',
-    'expired_at_date_format' => 'Y-m-d H:i:s',
+    'datetime_format' => 'Y-m-d H:i:s',
 ];
